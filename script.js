3076e57ee44efa49596d7b47e9b4d4ce1f2ec3e6
Max Fathauer <maxfathauer@gmail.com>
2022-04-02 22:52:04 -0400

    set up api calls

--- script.js
+++ script.js
@@ -1,7 +1,65 @@
 var formEl = document.querySelector('#form');
+var city = 'london';
+var state = "false";
+var country = "false";
+var cityUrl = "http://api.openweathermap.org/geo/1.0/direct?";
 
-var cityInput = function(event){
-    event.preventDefault();
-    const cityName = document.querySelector("input[name='city']").ariaValueMax;
-    document.append(cityName);
+var weatherUrl = 'https://api.openweathermap.org/data/2.5/onecall?';
+var current = {};
+var hourly
+
+kei="c65ea3e885f42ab98bbccee90b7f97e5";
+
+
+
+var lati = '0';//latitude named this way to avoid erron in calling for response in api
+var longe = '0';//longitude se above
+
+var response = new Object();
+
+//function to get latitude and longitude of city by the inputs name, state, and/or country
+var getLatLon = function(city, state, country){
+    let temp;
+    if(state != 'false'){
+        $.ajax({
+            url:cityUrl+'q='+city+','+state+'&limit=1&appid='+kei,
+            success:function(data){
+                lati = data[0].lat;
+                longe = data[0].lon;
+                getWeather(lati, longe);
+            }
+        });
+    }
+    else if( country != 'false'){
+        $.ajax({
+            url:cityUrl+'q='+city+','+country+'&limit=1&appid='+kei,
+            success:function(data){
+                lati = data[0].lat;
+                longe = data[0].lon;
+                getWeather(lati, longe);
+            }
+        });
+    }
+    else{
+        $.ajax({
+            url:cityUrl+'q='+city+'&limit=1&appid='+kei,
+            success:function(data){
+                lati = data[0].lat;
+                longe = data[0].lon;
+                getWeather(lati, longe);
+            }
+        });
+    }
+    
+};
+
+getLatLon(city, state, country);
+
+function getWeather(lati, longe){
+    $.ajax({
+        url:weatherUrl+'lat='+lati+'&lon='+longe+'&units=imperial&appid='+kei,
+        success:function(data){
+            console.log(data);
+        }
+    });
 }
