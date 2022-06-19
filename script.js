557057d81646ab1aefc6d5e9dbdb67fb83eaaac5
Max Fathauer <maxfathauer@gmail.com>
2022-06-19 18:12:38 -0400

    added history col

--- script.js
+++ script.js
@@ -1,25 +1,32 @@
 var formEl = document.querySelector('#form');
-var city = 'london';
-var state = "false";
-var country = "false";
+var city;
+var state;
+var country;
 var cityUrl = "http://api.openweathermap.org/geo/1.0/direct?";
-
+var historyCol = document.getElementById("history");
 var weatherUrl = 'https://api.openweathermap.org/data/2.5/onecall?';
 var current = {};
 var hourly
 
 kei="c65ea3e885f42ab98bbccee90b7f97e5";
 
+formEl.addEventListener("submit", function(event){
+    event.preventDefault();
+    city = document.querySelector("input[name='city']").value;
+    state = document.querySelector("input[name='state']").value;
+    country = document.querySelector("input[name='country']").value;
+    getLatLon(city, state, country);   
+})
 
-
-var lati = '0';//latitude named this way to avoid erron in calling for response in api
+var lati = '0';//latitude named this way to avoid err on in calling for response in api
 var longe = '0';//longitude se above
 
 var response = new Object();
 
 //function to get latitude and longitude of city by the inputs name, state, and/or country
 var getLatLon = function(city, state, country){
-    let temp;
+    historySearch(city);
+
     if(state != 'false'){
         $.ajax({
             url:cityUrl+'q='+city+','+state+'&limit=1&appid='+kei,
@@ -53,7 +60,12 @@ var getLatLon = function(city, state, country){
     
 };
 
-getLatLon(city, state, country);
+function historySearch(city){
+    let liEl = document.createElement("li");
+    liEl.className = "city";
+    liEl.innerHTML= city;
+    historyCol.appendChild(liEl);
+}
 
 function getWeather(lati, longe){
     $.ajax({
